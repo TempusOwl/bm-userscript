93499547003b0a5696a471d5f2f8f8bf77ca9054
orion <orion@syfusion.com>
2024-10-15 18:50:02 -0400

    Action Test

---
+++ core/userscript-template.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name Battlemetrics Toolkit - Desktop BETA TEST
+// @namespace https://www.battlemetrics.com/
+// @version 10.14
+// @updateURL https://raw.githubusercontent.com/TempusOwl/bm-userscript/refs/heads/beta/bm-toolkit-desktop.min.js
+// @downloadURL https://raw.githubusercontent.com/TempusOwl/bm-userscript/refs/heads/beta/bm-toolkit-desktop.min.js
+// @description Modifies the rcon panel for battlemetrics to help color code important events and details about players.
+// @author TempusOwl
+// @match https://www.battlemetrics.com/*
+// @match https://www.battlemetrics.com
+// @icon https://www.google.com/s2/favicons?sz=64&domain=battlemetrics.com
+// @grant GM_addStyle
+// @run-at document-end
+// ==/UserScript==
